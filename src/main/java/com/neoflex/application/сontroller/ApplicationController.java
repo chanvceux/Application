fb48b2ab3483d8ff8676f3e2ab042b788e8d5a2a
chanvceux <valentina.tev@yandex.ru>
2022-10-10 18:21:08 +0300

    added swagger

--- src/main/java/com/neoflex/application/сontroller/ApplicationController.java
+++ src/main/java/com/neoflex/application/сontroller/ApplicationController.java
@@ -1,6 +1,7 @@
 package com.neoflex.application.сontroller;
 import com.neoflex.application.dto.LoanApplicationRequestDTO;
 import com.neoflex.application.dto.LoanOfferDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,11 +22,13 @@ public class ApplicationController {
     }
 
     @PostMapping("/application")
+    @Operation(description = "Creating offers, interaction with method \"offersDeal\" from Deal")
     public List<LoanOfferDTO> offers(@Valid @RequestBody LoanApplicationRequestDTO loanApplicationRequestDTO) {
         return applicationService.offers(loanApplicationRequestDTO);
     }
 
     @PutMapping("/application/offer")
+    @Operation(description = "Saving offers to PostgreSQL database, interaction with method \"addOffer\" from Deal")
     public void offerCalculation(@RequestBody LoanOfferDTO loanOfferDTO) {
         applicationService.offerCalculation(loanOfferDTO);
     }
