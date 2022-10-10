717a588b70f6552efa03f130dbfec76a7bdf6dfd
chanvceux <valentina.tev@yandex.ru>
2022-10-10 18:26:37 +0300

    created logs

--- src/main/java/com/neoflex/application/service/ApplicationServiceImpl.java
+++ src/main/java/com/neoflex/application/service/ApplicationServiceImpl.java
@@ -3,16 +3,16 @@ package com.neoflex.application.service;
 import com.neoflex.application.dto.LoanApplicationRequestDTO;
 import com.neoflex.application.dto.LoanOfferDTO;
 import com.neoflex.application.feign_client.DealMC;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class ApplicationServiceImpl {
-
     private final DealMC dealMC;
-
     @Autowired
     public ApplicationServiceImpl(DealMC dealMC) {
         this.dealMC = dealMC;
@@ -20,10 +20,12 @@ public class ApplicationServiceImpl {
 
     public List<LoanOfferDTO> offers(LoanApplicationRequestDTO loanApplicationRequestDTO) {
         List<LoanOfferDTO> offers = dealMC.offersDeal(loanApplicationRequestDTO);
+        log.trace("GETTING LoanApplicationRequestDTO: {}, RETURNING offers FROM MC DEAL: {}", loanApplicationRequestDTO, offers);
         return offers;
     }
 
     public void offerCalculation(LoanOfferDTO loanOfferDTO) {
+        log.trace("GETTING LoanOfferDTO, VALUE: {}, CREATING OFFERS: MC DEAL", loanOfferDTO);
         dealMC.addOffer(loanOfferDTO);
     }
 }
--- src/main/java/com/neoflex/application/сontroller/ApplicationController.java
+++ src/main/java/com/neoflex/application/сontroller/ApplicationController.java
@@ -1,8 +1,10 @@
 package com.neoflex.application.сontroller;
 import com.neoflex.application.dto.LoanApplicationRequestDTO;
 import com.neoflex.application.dto.LoanOfferDTO;
+import com.neoflex.application.service.ApplicationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.neoflex.application.service.ApplicationServiceImpl;
@@ -11,6 +13,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @Tag(name = "ApplicationController", description = "Interaction with MVP2")
+@Slf4j
 @RestController
 public class ApplicationController {
 
@@ -24,12 +27,15 @@ public class ApplicationController {
     @PostMapping("/application")
     @Operation(description = "Creating offers, interaction with method \"offersDeal\" from Deal")
     public List<LoanOfferDTO> offers(@Valid @RequestBody LoanApplicationRequestDTO loanApplicationRequestDTO) {
-        return applicationService.offers(loanApplicationRequestDTO);
+        List<LoanOfferDTO> offers = applicationService.offers(loanApplicationRequestDTO);
+        log.trace("GETTING LoanApplicationRequestDTO: {}, RETURNING offers: {}", loanApplicationRequestDTO, offers);
+        return offers;
     }
 
     @PutMapping("/application/offer")
     @Operation(description = "Saving offers to PostgreSQL database, interaction with method \"addOffer\" from Deal")
     public void offerCalculation(@RequestBody LoanOfferDTO loanOfferDTO) {
+        log.trace("GETTING LoanOfferDTO, VALUE: {}", loanOfferDTO);
         applicationService.offerCalculation(loanOfferDTO);
     }
 }
